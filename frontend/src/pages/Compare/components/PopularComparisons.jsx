f9f33c2bdf3525a49b14ee73d35cf83c35a3033c
Barash <barishm@outlook.com>
2024-03-08 01:08:59 +0200

    popular comparisons div fixed

--- frontend/src/pages/Compare/components/PopularComparisons.jsx
+++ frontend/src/pages/Compare/components/PopularComparisons.jsx
@@ -1,10 +1,27 @@
 import { useGetAllMostComparesQuery } from "../../../app/services/mostComparesApiSlice";
+import { useLazyGetRobotByIdQuery } from "../../../app/services/robotApiSlice";
 import Loading from "../../../components/Loading";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addRobot,deleteAllRobots } from "../../../app/redux/compareSlice";
 
 const PopularComparisons = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     const lang = useSelector((state) => state.language.lang);
     const { data: allCompares, isLoading: allComparesIsLoading } = useGetAllMostComparesQuery();
+    const [trigger] = useLazyGetRobotByIdQuery();
+    const handleComparisonClick = (id1, id2, id3) => {
+        const ids = [id1, id2, id3].filter(Boolean);
+        dispatch(deleteAllRobots());
+        ids.forEach((id) => {
+          trigger({id}).then((response) => {
+            dispatch(addRobot(response.data));
+          });
+        });
+        navigate("/compare");
+    };
 
     return (
         <div style={{ marginTop: "40px",marginBottom:"50px", marginRight: "auto", marginLeft: "auto", textAlign: "center" }}>
@@ -12,7 +29,7 @@ const PopularComparisons = () => {
             {allComparesIsLoading ? <Loading /> :
                 <div>
                     {allCompares.map((item) => (
-                        <div key={item.id} data-id1={item.robot1?.id} data-id2={item.robot2?.id} data-id3={item.robot3?.id} className="card p-4 mt-3" style={{minHeight:"50px",maxWidth:"450px", marginRight:"auto",marginLeft:"auto",border:"none"}}>
+                        <div key={item.id} onClick={() => handleComparisonClick(item.robot1?.id, item.robot2?.id, item.robot3?.id)} className="card p-4 mt-3" style={{minHeight:"50px",maxWidth:"450px", marginRight:"auto",marginLeft:"auto",border:"none", cursor: 'pointer' }}>
                             <h6 className="card-title">{item.robot1?.model} <span style={{color:"gray"}}>vs.</span> {item.robot2?.model} {item.robot3 && (<span style={{color:"gray"}}>vs.</span>)} {item.robot3?.model}</h6>
                         </div>
                     ))}
--- frontend/src/pages/Home/components/YoutubeVideos.jsx
+++ frontend/src/pages/Home/components/YoutubeVideos.jsx
@@ -5,12 +5,16 @@ const YoutubeVideos = () => {
   const lang = useSelector((state) => state.language.lang);
 
   return (
-    <div style={{marginBottom:"50px"}}>
+    <div style={{ marginBottom: "50px" }}>
       <h3
         className="fw-bolder"
         style={{ marginTop: "40px", textAlign: "center" }}
       >
-        {lang === "en" ? <>Latest Youtube Videos</> : <>Наскоро качени видеоклипове</>}
+        {lang === "en" ? (
+          <>Latest Youtube Videos</>
+        ) : (
+          <>Наскоро качени видеоклипове</>
+        )}
       </h3>
       {isLoading ? (
         <>Loading...</>
@@ -21,7 +25,7 @@ const YoutubeVideos = () => {
               <iframe
                 width="400"
                 height="225"
-                style={{borderRadius:"10px"}}
+                style={{ borderRadius: "10px" }}
                 src={`https://www.youtube.com/embed/${item.id.videoId}`}
               ></iframe>
             </div>
